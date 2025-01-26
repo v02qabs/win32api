8b5030443b3543fdbe223386d145a4414902406e
v02qabs <takesue090@gmail.com>
2025-01-26 16:27:41 +0900

    wm_command and wm_create

--- diary/diary.c
+++ diary/diary.c
@@ -1,103 +1,33 @@
 #include<windows.h>
 #include<stdlib.h>
+#include <stdio.h>
 
-#define BUTTON_ID1 0
-#define GetHInstance( ) ((HINSTANCE)GetModuleHandle(0))
+#include "write_diary.h"
 
-<<<<<<< HEAD
 LRESULT CALLBACK WndProc(HWND hwnd , UINT msg , WPARAM wp , LPARAM lp) {
+  static HWND hButton;
 	switch (msg) {
-	case WM_DESTROY:
-		exit(0);
-		return 0;
+	        case WM_CREATE:
+            // ボタンの作成
+            hButton = CreateWindow(
+                "BUTTON", "Click Me",
+                WS_CHILD | WS_VISIBLE,
+                100, 100, 100, 30,
+                hwnd, (HMENU)1, NULL, NULL
+            );
+            break;
 	case WM_COMMAND:
-		switch(LOWORD(wp))
-		{
-			case XBUTTON1:
-					MessageBox(hwnd, TEXT("BUTTON_ID1"), TEXT("ID_BUTTON_1"), MB_OK);
-					ShowWindow(hwnd, SW_HIDE);
-					
-					break;
-			case XBUTTON2:
-					MessageBox(hwnd, TEXT("BUTTON_ID2"), TEXT("BUTTON 2"), MB_OK);
-					break;
-		}
-		return 0;
-		
-	}
-	return DefWindowProc(hwnd , msg , wp , lp);
-}
-
-
-int WINAPI WinMain(HINSTANCE hInstance , HINSTANCE hPrevInstance ,
-		 PSTR lpCmdLine , int nCmdShow ) {
-	HWND hwnd;
-=======
-int edit_and_writting()
-{
-	HWND hwnd1;
->>>>>>> 9b7df2b594147598804ff10ae42efd3334fabf7f
-	MSG msg;
-	WNDCLASS winc;
-	winc.style = CS_HREDRAW | CS_VREDRAW;
-	winc.lpfnWndProc = NULL;
-	winc.cbClsExtra = winc.cbWndExtra = 0;
-	winc.hInstance = GetHInstance();
-	winc.hCursor = LoadCursor(NULL , IDC_ARROW);
-	winc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	winc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	winc.lpszMenuName = NULL;
-	winc.lpszClassName = TEXT("KITTY");
-	if (!RegisterClass(&winc)) return 0;
-
-	hwnd1 = CreateWindow(
-			TEXT("diary") , TEXT("my diary") ,
-			WS_OVERLAPPEDWINDOW  | WS_VISIBLE ,
-			100 , 100 , 200 , 200 , NULL , NULL ,
-			GetHInstance() , NULL
-	);
-<<<<<<< HEAD
-	
-	CreateWindow(TEXT("button") , TEXT("WRITE DIARY"),
-		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 
-		0,0,100,30, hwnd, (HMENU)XBUTTON1, hInstance, NULL);
-
-	CreateWindow(TEXT("button") , TEXT("VIEW DIARY"),
-		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 
-		0,35,100,30, hwnd, (HMENU)XBUTTON2, hInstance, NULL);
-
-
-	if (hwnd == NULL) return 0;
-=======
-		
-	if (hwnd1 == NULL) return 0;
->>>>>>> 9b7df2b594147598804ff10ae42efd3334fabf7f
-
-	while (TRUE) {
-		GetMessage(&msg , NULL , 0 , 0);
-		DispatchMessage(&msg);
-	}
-	return 0;
-
-}
-
-
-
-LRESULT CALLBACK WndProc(HWND hwnd , UINT msg , WPARAM wp , LPARAM lp) {
-	switch (msg) {
-	case WM_DESTROY:
+            if (LOWORD(wp) == 1) { // ボタンがクリックされた場合
+                 MessageBox(hwnd, "Button Clicked!", "Message", MB_OK);
+            }
+            break;
+		case WM_DESTROY:
 		exit(0);
 		return 0;
-	
-	case  WM_COMMAND:
-		switch(LOWORD(wp)){
-			case BUTTON_ID1:
-			MessageBox(hwnd, TEXT("BUTTON"),  TEXT("BUTTON"), MB_OK);
-			edit_and_writting();
-			break;
-		}
-		return 0;
 	}
+
+
+
 
 	return DefWindowProc(hwnd , msg , wp , lp);
 }
@@ -108,11 +38,6 @@ int WINAPI WinMain(HINSTANCE hInstance , HINSTANCE hPrevInstance ,
 	HWND hwnd;
 	MSG msg;
 	WNDCLASS winc;
-	if(!CreateDirectory("c:\\my-diary", NULL))
-	{
-		MessageBox(
-			NULL, TEXT("Can't make directory."), TEXT("error"), MB_OK);
-	}
 
 	winc.style		= CS_HREDRAW | CS_VREDRAW;
 	winc.lpfnWndProc	= WndProc;
@@ -122,21 +47,21 @@ int WINAPI WinMain(HINSTANCE hInstance , HINSTANCE hPrevInstance ,
 	winc.hCursor		= LoadCursor(NULL , IDC_ARROW);
 	winc.hbrBackground	= (HBRUSH)GetStockObject(WHITE_BRUSH);
 	winc.lpszMenuName	= NULL;
-	winc.lpszClassName	= TEXT("diary");
+	winc.lpszClassName	= TEXT("KITTY");
 
 	if (!RegisterClass(&winc)) return 0;
 
 	hwnd = CreateWindow(
-			TEXT("diary") , TEXT("my diary") ,
+			TEXT("KITTY") , TEXT("Kitty on your lap") ,
 			WS_OVERLAPPEDWINDOW  | WS_VISIBLE ,
 			100 , 100 , 200 , 200 , NULL , NULL ,
 			hInstance , NULL
 	);
-	
-	CreateWindow(TEXT("button") , TEXT("WRITE DIARY"),
-		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 
-		0,0,100,30, hwnd, (HMENU)BUTTON_ID1 , hInstance, NULL);
-		
+
+
+	CreateWindow(TEXT("BUTTON"),TEXT("OK"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 0,0,100,30, hwnd,(HMENU)1,hInstance, NULL);
+
+
 	if (hwnd == NULL) return 0;
 
 	while (TRUE) {
